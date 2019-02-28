6c316a9b0617e5acec1907172457aef84ea6ff2a
Yuen92 <bianconithomas@gmail.com>
2019-02-28 01:11:21 +0100

    Remove unused css

--- src/styles/home-styles-images.js
+++ src/styles/home-styles-images.js
@@ -4,16 +4,18 @@ export const HomeStylesImages = html`
 <style>
   /* 320x635 */
   section.first{
-    background-image: image-set(url(./images/320w/ddti_960w_1x.webp) 1x, url(./images/320w/ddti_960w_1x.jpg) 1x);
-    background-image: -webkit-image-set(url(./images/320w/ddti_960w_1x.webp) 1x, url(./images/320w/ddti_960w_1x.jpg) 1x);
+    background-image: url(./images/320w/ddti_960w_1x.jpg);
+    background-image: -webkit-image-set(url(./images/320w/ddti_960w_1x.webp) 1x, url(./images/320w/ddti_960w_1x.jpg) 2x);
     background-size: cover;
   }
   
   @media (min-width: 321px){
     /* 640x400 */
+    /* Google Page Speed on mobile choose to load jpg instead of webp. Don't know why.
+    /* https://developers.google.com/speed/pagespeed/insights/?hl=fr&url=https%3A%2F%2Fwww.thomas-bianconi.com */
     section.first{
-      background-image: image-set(url(./images/640w/ddti_960w_1x.webp) 1x, url(./images/640w/ddti_960w_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/640w/ddti_960w_1x.webp) 1x, url(./images/640w/ddti_960w_1x.jpg) 1x);
+      background-image: url(./images/640w/ddti_960w_1x.jpg);
+      background-image: -webkit-image-set(url(./images/640w/ddti_960w_1x.webp) 1x, url(./images/640w/ddti_960w_1x.jpg) 2x);
       background-size: cover;
     }
   }
@@ -21,39 +23,39 @@ export const HomeStylesImages = html`
   @media (min-width: 641px){
     /* 960x350 */
     section.first{
-      background-image: image-set(url(./images/960w/ddti_960w_1x.webp) 1x, url(./images/960w/ddti_960w_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/960w/ddti_960w_1x.webp) 1x, url(./images/960w/ddti_960w_1x.jpg) 1x);
+      background-image: url(./images/960w/ddti_960w_1x.jpg);
+      background-image: -webkit-image-set(url(./images/960w/ddti_960w_1x.webp) 1x, url(./images/960w/ddti_960w_1x.jpg) 2x);
       background-size: cover;
     }
   }
   @media (min-width: 961px){
     /* 1280x350 */
     section.first{
-      background-image: image-set(url(./images/1280w/ddti_1280w_1x.webp) 1x, url(./images/1280w/ddti_1280w_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/1280w/ddti_1280w_1x.webp) 1x, url(./images/1280w/ddti_1280w_1x.jpg) 1x);
+      background-image: url(./images/1280w/ddti_1280w_1x.jpg);
+      background-image: -webkit-image-set(url(./images/1280w/ddti_1280w_1x.webp) 1x, url(./images/1280w/ddti_1280w_1x.jpg) 2x);
       background-size: cover;
     }
   }
   @media (min-width: 1281px){
     /* 1600x350 */
     section.first{
-      background-image: image-set(url(./images/1600w/ddti_1600w_1x.webp) 1x, url(./images/1600w/ddti_1600w_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/1600w/ddti_1600w_1x.webp) 1x, url(./images/1600w/ddti_1600w_1x.jpg) 1x);
+      background-image: url(./images/1600w/ddti_1600w_1x.jpg);
+      background-image: -webkit-image-set(url(./images/1600w/ddti_1600w_1x.webp) 1x, url(./images/1600w/ddti_1600w_1x.jpg) 2x);
       background-size: cover;
     }
   }
   @media (min-width: 1601px){
     /* 1920x350 */
     section.first{
-      background-image: image-set(url(./images/1920w/ddti_1x.webp) 1x, url(./images/1920w/ddti_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/1920w/ddti_1x.webp) 1x, url(./images/1920w/ddti_1x.jpg) 1x);
+      background-image: url(./images/1920w/ddti_1x.jpg);
+      background-image: -webkit-image-set(url(./images/1920w/ddti_1x.webp) 1x, url(./images/1920w/ddti_1x.jpg) 2x);
       background-size: cover;
     }
   }
   @media (min-width: 1921px){
     section.first{
-      background-image: image-set(url(./images/max/ddti_1x.webp) 1x, url(./images/max/ddti_1x.jpg) 1x);
-      background-image: -webkit-image-set(url(./images/max/ddti_1x.webp) 1x, url(./images/max/ddti_1x.jpg) 1x);
+      background-image: url(./images/max/ddti_1x.jpg);
+      background-image: -webkit-image-set(url(./images/max/ddti_1x.webp) 1x, url(./images/max/ddti_1x.jpg) 2x);
       background-size: cover;
     }
   }
